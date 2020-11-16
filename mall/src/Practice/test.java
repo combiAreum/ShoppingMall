0cf79b833cb7ba81cb4f49e2ac9c8c5895c55e74
combiAreum <74331466+combiAreum@users.noreply.github.com>
2020-11-16 16:48:45 +0900

    Add files via upload

--- mall/src/Practice/test.java
+++ mall/src/Practice/test.java
@@ -8,49 +8,49 @@ public class test {
 		
 		String[] current = new String[11];
 		
-		current[0] = "main";
-		current[1] = "join1";
-		current[2] = "join2";
-		current[3] = "join3";
-		current[4] = "login";
-		current[5] = "mypage";
-		current[6] = "main1";
-		current[7] = "main2";
-		current[8] = "main3";
-		current[9] = "logout";
-		current[10] = "areum";
-		
-		
-		int index = 0;
-		while(true) {
-			System.out.print(current[index] + "\n");
-			index += 1;
-			if(index == current.length-1) {
-			
-				break;
-			}
-		}
-		
-		index = 0;
-		for(;true;) {
-			System.out.print(current[index] + "\n");
-			index += 1;
-			if(index == current.length-1) {
-			
-				break;
-			}
-		}
-		
-		index = 0;
-		while(index<current.length-1) {
-			System.out.print(current[index] + "\n");
-			index += 1;
-		}
-		
-		
-		for(index=0; index<current.length-1; index++) {
-			System.out.print(current[index] + "\n");
-		}
-	
+//		current[0] = "main";
+//		current[1] = "join1";
+//		current[2] = "join2";
+//		current[3] = "join3";
+//		current[4] = "login";
+//		current[5] = "mypage";
+//		current[6] = "main1";
+//		current[7] = "main2";
+//		current[8] = "main3";
+//		current[9] = "logout";
+//		current[10] = "areum";
+//		
+//		
+//		int index = 0;
+//		while(true) {
+//			System.out.print(current[index] + "\n");
+//			index += 1;
+//			if(index == current.length-1) {
+//			
+//				break;
+//			}
+//		}
+//		
+//		index = 0;
+//		for(;true;) {
+//			System.out.print(current[index] + "\n");
+//			index += 1;
+//			if(index == current.length-1) {
+//			
+//				break;
+//			}
+//		}
+//		
+//		index = 0;
+//		while(index<current.length-1) {
+//			System.out.print(current[index] + "\n");
+//			index += 1;
+//		}
+//		
+//		
+//		for(index=0; index<current.length-1; index++) {
+//			System.out.print(current[index] + "\n");
+//		}
+//	
 	}
 }
--- mall/src/view/FrontEnd.java
+++ mall/src/view/FrontEnd.java
@@ -3,32 +3,65 @@ package view;
 import java.util.Scanner;
 
 public class FrontEnd {
-
 	//생성자 호출
 	public FrontEnd() {
-		this.viewControl(this.makeTitle());
-				
+		this.viewControl(this.makeTitle());		
 	}
-	
-	
+		
 	private void viewControl(String title) {
+		
+		int menuCode = 0;
+		
+//		String[][] menu = new String[3][];
+//		
+//		String[] menu1 = new String[4];
+//		menu1[0] = "Main";
+//		menu1[1] = "로그인";
+//		menu1[2] = "회원가입";
+//		menu1[3] = "종료";
+//		menu[0] = menu1;
+//		
+//		String[] menu2 = new String[1];
+//		menu2[0] = "로그인";
+//		menu[1] = menu2;
+//		
+//		String[] menu3 = new String[3];
+//		menu3[0] = "회원가입";
+//		menu3[1] = "개인회원";
+//		menu3[2] = "기업회원";
+//		menu[2] = menu3;
+		
+//		위의 것 이렇게 바꿀수 있음!
+		String[][] menu = {{"MAIN", "LogIn", "Join", "Close"},
+						   {"LogIn"},
+						   {"Join", "Personal", "Company", "Back"}};
+		
 		while(true) {
-			
-			this.screenview(title);
-			this.screenview(this.makeSubTitle());
-			this.screenview(this.makeMenu());
-			
-			if(this.clientInput().equals("0")) {
+			this.print(title);
+			this.print(this.printMenu(menu, menuCode));
+
+			menuCode = Integer.parseInt(this.clientInput());
+
+			if(menuCode == 0)
 				break;
+			
+			while(true) {
+				this.print(title);
+				this.print(this.printMenu(menu, menuCode));
+
+				menuCode = Integer.parseInt(this.clientInput());
+				
+				if(menuCode == 0) 
+					break;
 			}
-		}
-		this.screenview("종료됨.");
+					
+		}	
+		this.print("종료됨.");
 	}
-	
+
 
 	private String makeTitle() {
-		
-		StringBuffer sb= new StringBuffer();
+		StringBuffer sb = new StringBuffer();
 		sb.append("ᕦ(ò_óˇ)ᕤ ________________________________\n\n");
 		sb.append("Second Project :: Shopping Mall\n");
 		sb.append("   Designed by AREUM\n\n");
@@ -37,19 +70,28 @@ public class FrontEnd {
 		return sb.toString();
 	}
 	
-	private String makeSubTitle() {
+	private String printMenu(String[][] menu, int menuCode) {
 		
-		StringBuffer sb= new StringBuffer();
-		sb.append("[ JOIN :: STEP 1 ]\n");
+		int index = 0;
+		StringBuffer sb = new StringBuffer();
+		sb.append("[" + menu[menuCode][index] + "]\n");
 		
-		return sb.toString();
-	}
-	
-	private String makeMenu() {
-		StringBuffer sb= new StringBuffer();
-		sb.append("------------------------------------\n");
-		sb.append("1. 로그인         2. 회원가입        0.종료\n");
-		sb.append("----------------------------Select :  ");
+		if(menu[menuCode].length != 1) {
+			sb.append("------------------------------------\n");
+			for(index=1; index<menu[menuCode].length-1; index++) {
+
+				sb.append(index + "." + menu[menuCode][index]);
+
+				if(index % 2 == 0 ) {
+					sb.append("\n");
+				}
+				else{
+					sb.append("   ");
+				}
+			}
+			sb.append( 0 + "." + menu[menuCode][menu[menuCode].length-1] +"\n");
+			sb.append("------------------------------ Select :  \n");
+		}
 		return sb.toString();
 	}
 	
@@ -59,8 +101,8 @@ public class FrontEnd {
 		return scanner.next();
 	}
 	
-	private void screenview(String box) {
-		System.out.print(box);
+	private void print(String text) {
+		System.out.print(text);
 	}
 	
 }
