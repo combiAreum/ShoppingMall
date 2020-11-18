56c5c138db55d19c0b11ebc5a96fac0fb9af4993
combiAreum <74331466+combiAreum@users.noreply.github.com>
2020-11-18 10:31:50 +0900

    join

--- mall/src/back/controller/FrontController.java
+++ mall/src/back/controller/FrontController.java
@@ -1,8 +1,32 @@
 package back.controller;
 
 public class FrontController {
+	
+	private String[][] members = {{"a", "아름","1234","P"},
+								  {"b", "원빈","1234","P"},
+								  {"c", "혜지","1234","P"},
+								  {"d", "민성","1234","P"},
+								  {"e", "선아","1234","P"}};
 
 	public FrontController() {
 		
+	}
+	
+	public String[] duplicated(String[] memberInfo) {
+		String[] idCheck = new String[1];
+		
+		// "1" = 사용가능(초기값)
+		idCheck[0] = "1";
+		
+		//id추출해서 데이터베이스에 있는 id와 비교
+		for(int index = 0; index < members.length; index++) {
+			if(memberInfo[0].equals(members[index][0])) {
+				
+				// "0" = 사용불가능(id중복됨)
+				idCheck[0] = "0";
+				break;
+			}
+		}
+		return idCheck;
 	}
 }
--- mall/src/view/FrontEnd.java
+++ mall/src/view/FrontEnd.java
@@ -2,97 +2,148 @@ package view;
 
 import java.util.Scanner;
 
+import back.controller.FrontController;
+
 public class FrontEnd {
+	
+	//필드
+	private FrontController fc;
+	
 	//생성자 호출
 	public FrontEnd() {
+		
+		//
+		fc = new FrontController();
+		
 		this.viewControl(this.makeTitle());		
 	}
 		
+	
+	
 	private void viewControl(String title) {
 		
 		int menuCode = 0;
 		
-//		String[][] menu = new String[3][];
-//		
-//		String[] menu1 = new String[4];
-//		menu1[0] = "Main";
-//		menu1[1] = "로그인";
-//		menu1[2] = "회원가입";
-//		menu1[3] = "종료";
-//		menu[0] = menu1;
-//		
-//		String[] menu2 = new String[1];
-//		menu2[0] = "로그인";
-//		menu[1] = menu2;
-//		
-//		String[] menu3 = new String[3];
-//		menu3[0] = "회원가입";
-//		menu3[1] = "개인회원";
-//		menu3[2] = "기업회원";
-//		menu[2] = menu3;
-		
-//		위의 것 이렇게 바꿀수 있음!
 		String[][] menu = {{"MAIN", "LogIn", "Join", "Close"},
 						   {"LogIn"},
 						   {"Join", "Personal", "Company", "Back"}};
-		
+
 		while(true) {
 			this.print(title);
-			this.print(this.printMenu(menu, menuCode));
+			this.print(this.printMenu(true, menu, menuCode));
 
 			menuCode = Integer.parseInt(this.clientInput());
 
-			if(menuCode == 0)
+			if(menuCode == 0) {
 				break;
-			
-			while(true) {
-				this.print(title);
-				this.print(this.printMenu(menu, menuCode));
-
-				menuCode = Integer.parseInt(this.clientInput());
-				
-				if(menuCode == 0) 
-					break;
 			}
-					
+			else if(menuCode == 1) {
+				this.login(title);
+				menuCode = 0;
+			}
+			else if(menuCode == 2) {
+				this.join(title, menu, menuCode);
+			}
+			
+			}
+
 		}	
-		this.print("종료됨.");
+	
+	
+	private void login(String title) {
+		this.print(title);
+		this.print("로그인");
 	}
+	
+	private void join(String title, String[][] menu, int menuCode) {
+		
+		String[] userInfo = new String[4];
+		boolean idCheck = false;
+		int code;
+		
+		this.print(title);
+		while(!idCheck) {
+			this.print(this.makeInputItem(true, "Join", "USER ID" ));
+			userInfo[0] = this.clientInput();
+			//서버전송: id 중복여부체크
+			idCheck = (fc.duplicated(userInfo)[0].equals("0"))? false: true;
+			while(!idCheck) {
+				this.print(this.makeInputItem(false, "Join", "USER ID" ));
+				userInfo[0] = this.clientInput();
+				//서버전송: id 중복여부체크
+				idCheck = (fc.duplicated(userInfo)[0].equals("0"))? false: true;
+				
+			}
+		}
+		
+		this.print(this.makeInputItem(false, null, "USER NAME"));
+		userInfo[1] = this.clientInput();
+		
+		this.print(this.makeInputItem(false, null, "USER PW"));
+		userInfo[2] = this.clientInput();
+		
+		this.print(this.printMenu(false, menu, menuCode));
+		code = Integer.parseInt(this.clientInput());
+		if(code >= 1 && code <= menu[menuCode].length-2) {
+			userInfo[3] = (code==1? "P": "C"); 
+		}
+		
+		//서버전송
+		//서버로부터  가입된 회원정보를 리턴받아 화면출력
+		//사용자로부터 confirm 후 메인화면 복귀
+	}
+	
 
-
+	private String makeInputItem(boolean type, String step, String item) {
+		StringBuffer sb = new StringBuffer();
+			
+			sb.append(type? "[" + step + "]\n": "");
+			sb.append(type? "-------------------------------------------------------\n": "");
+			sb.append("[" + item + "]: ");
+			
+			return sb.toString();
+		}
+	
+	
+	private String makeResult(boolean type, String result) {
+		StringBuffer sb = new StringBuffer();
+		
+			sb.append("-------------------------------------------------------" + result);
+			sb.append(type? ":": "");
+			
+			return sb.toString();
+	}
+	
+	
 	private String makeTitle() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("ᕦ(ò_óˇ)ᕤ ________________________________\n\n");
-		sb.append("Second Project :: Shopping Mall\n");
-		sb.append("   Designed by AREUM\n\n");
+		sb.append("  Second Project :: Shopping Mall\n");
+		sb.append("  Designed by AREUM\n\n");
 		sb.append("_____________________________ᕦ(ò_óˇ)ᕤ\n");
-		sb.append("\n\n");
+		sb.append("\n");
 		return sb.toString();
 	}
 	
-	private String printMenu(String[][] menu, int menuCode) {
+	private String printMenu(boolean type, String [][] menu, int menuCode) {
 		
-		int index = 0;
 		StringBuffer sb = new StringBuffer();
-		sb.append("[" + menu[menuCode][index] + "]\n");
 		
-		if(menu[menuCode].length != 1) {
-			sb.append("------------------------------------\n");
-			for(index=1; index<menu[menuCode].length-1; index++) {
-
-				sb.append(index + "." + menu[menuCode][index]);
-
-				if(index % 2 == 0 ) {
-					sb.append("\n");
-				}
-				else{
-					sb.append("   ");
-				}
+		sb.append(type? ("[" + menu[menuCode][0]+"]\n"): "\n");
+		
+	
+		
+		if(menu[menuCode].length > 1) {
+			sb.append("-------------------------------------------------------\n");
+			for(int index=1; index < menu[menuCode].length-1; index++) {
+				sb.append("  " +index + ". " + menu[menuCode][index] + ((index%2 == 0)? "\n": "   "));
 			}
-			sb.append( 0 + "." + menu[menuCode][menu[menuCode].length-1] +"\n");
-			sb.append("------------------------------ Select :  \n");
+			sb.append("  " + "0. " + menu[menuCode][menu[menuCode].length-1] + "\n");
+			sb.append("------------------------------------------------------- Select : ");
 		}
+		
 		return sb.toString();
+		
 	}
 	
 	
